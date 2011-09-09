386ef9919e66ab87644ea9c92eb1344e32e1920a
Vincent Fourmond <vincent.fourmond@ifr88.cnrs-mrs.fr>
2011-09-09 11:54:25 +0200

    Bi-exponential fit with film loss

--- src/common-fits.cc
+++ src/common-fits.cc
@@ -157,6 +157,57 @@ public:
 Exp2Fit fit_expd2;
 
 
+class FilmExp2Fit : public FunctionFit {
+public:
+
+  /// Formula:
+  /// a[1] * exp(-(x - a[0]) / a[2]) + a[3] * exp(-(x - a[0]) / a[4]) + a[5];
+  ///
+  /// a[0] is a redundant parameter, but it can be of use to shift the
+  /// X axis without touching the original data.
+  virtual double function(const double * a, 
+                          FitData * , double x) {
+    return (a[1] * exp(-(x - a[0]) / a[2]) + a[3] * 
+            exp(-(x - a[0]) / a[4]) + a[5]) * 
+      exp(-a[6] * (x - a[0]));
+  };
+
+  virtual void initialGuess(FitData * , 
+                            const DataSet * ds,
+                            double * a)
+  {
+    a[0] = ds->x()[0];          // x0 = x[0]
+    a[5] = ds->y().last();      
+    a[1] = 0.5*(ds->y()[0] - a[3]);
+    a[2] = (ds->x().last() - a[0])/20;
+    a[3] = a[1];
+    a[4] = (ds->x().last() - a[0])/3;
+    a[6] = 1e-3/(ds->x().last() - a[0]);
+  };
+
+  virtual QList<ParameterDefinition> parameters() const {
+    return QList<ParameterDefinition>()
+      << ParameterDefinition("x0", true)
+      << ParameterDefinition("A1")
+      << ParameterDefinition("tau1")
+      << ParameterDefinition("A2")
+      << ParameterDefinition("tau2")
+      << ParameterDefinition("B")
+      << ParameterDefinition("kloss");
+  };
+
+
+  FilmExp2Fit() : FunctionFit("film-expd2", 
+                              "Bi-exponential decay with film loss",
+                              "Bi-exponential decay with film loss, formula is :...") 
+  { ;};
+};
+
+// DO NOT FORGET TO CREATE AN INSTANCE OF THE CLASS !!
+// Its name doesn't matter.
+FilmExp2Fit fit_film_expd2;
+
+
 /// @todo Ideas:
 /// @li fits with arbitrary number of exponentials (using a number
 /// parameter) ?

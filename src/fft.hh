8144bf7c90bd8a01d40fbc6bbc4d8029ad4f300b
Vincent <vincent.fourmond@9online.fr>
2012-01-01 16:53:37 +0100

    (less skinny) draft for a FFT implementation

--- src/fft.hh
+++ src/fft.hh
@@ -22,10 +22,50 @@
 
 #include <vector.hh>
 
+#include <gsl/gsl_fft_real.h>
+#include <gsl/gsl_fft_halfcomplex.h>
+
 /// This class provides fourier transforms facilities (including
 /// filtering)
 class FFT {
+
+  /// The distance between two points from the normal space
+  double deltaX;
+
+  /// Original Y data
+  Vector normal;
+
+  /// Transformed data
+  Vector ffted;
+
+  /// Whether we have normal data
+  bool unFftDone;
+
+
+  gsl_fft_real_wavetable * realWT;
+  gsl_fft_halfcomplex_wavetable * hcWT;
+  gsl_fft_real_workspace * fftWS;
+
+  
+  /// Whether we use a cubic baseline or not
+  bool useCubicBase;
+  
+  /// Coefficients of a cubic baseline.
+  double bl[4];
+
+  /// Sets up the mem allocation + computes the transform
+  void setup(double dx, const Vector & y, bool cub);
+
+  /// Frees all associated memory
+  void free();
+
 public:
+
+  /// Builds a FFT object from X and Y things.
+  ///
+  /// Raises an exception if the points are not equally spaced.
+  FFT(const Vector & x, const Vector & y, bool cub = true);
+  FFT(double dx, const Vector & y, bool cub = true);
 };
 
 #endif

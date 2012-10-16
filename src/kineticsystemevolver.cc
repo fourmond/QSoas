e1766bdcf28cb1a803e55144675c7bb708ed33e2
Vincent Fourmond <vincent.fourmond@imm.cnrs.fr>
2012-10-16 16:39:32 +0200

    Full time-dependence for kinetic fits

--- src/kineticsystemevolver.cc
+++ src/kineticsystemevolver.cc
@@ -221,21 +221,167 @@ kineticSystem("kinetic-system", // command name
 
 //////////////////////////////////////////////////////////////////////
 
+/// This small represents a dependence on time of a parameter. It is
+/// for now very basic. It is made of several bits that add up to
+/// one. All bits have independent amplitude, but when they have other
+/// accessory parameters (time constant ?), those can either be shared
+/// or independent.
+///
+/// @questions Maybe this has to be turned into a general virtual
+/// hierarchy ?
+class TimeDependentParameter {
+public:
+
+  typedef enum {
+    Exponential,
+  } Type;
+
+  /// What kind of funtion is that ?
+  Type type;
+  
+  /// The number of bits
+  int number;
+
+  /// Whether the bits have independent accessory parameters or not
+  bool independentBits;
+
+  /// The base index of the parameters. @hack Mutable so that it can
+  /// be updated from the const functions.
+  int baseIndex;
+
+
+  /// The number of parameters
+  int parameterNumber() {
+    switch(type) {
+    case Exponential:
+      return number*2 + (independentBits ? number : 1);
+    default:
+      return 0;
+    };
+    return 0;                   // Not needed in principle.
+  };
+
+  /// Parameter definitions
+  QList<ParameterDefinition> parameters(const QString & prefix) {
+    QList<ParameterDefinition> ret;
+    switch(type) {
+    case Exponential:
+      if(! independentBits)
+        ret << ParameterDefinition(prefix + "_tau");
+      for(int i = 0; i < number; i++) { 
+        ret << ParameterDefinition(QString("%2_%1").
+                                   arg(i+1).arg(prefix), true);
+        ret << ParameterDefinition(QString("%2_t_%1").
+                                   arg(i+1).arg(prefix), true);
+        if(independentBits)
+          ret << ParameterDefinition(QString("%2_tau_%1").
+                                     arg(i+1).arg(prefix));
+      }
+      break;
+    default:
+      ;
+    };
+    return ret;
+  };
+
+  /// Returns the value at the given time...
+  double computeValue(double t, const double * parameters) const {
+    double value = 0;
+    switch(type) {
+    case Exponential:
+      for(int i = 0; i < number; i++) {
+        double t0   = parameters[baseIndex + (independentBits ? i*3+1 : 2*i+2)];
+        double conc = parameters[baseIndex + (independentBits ? i*3   : 2*i+1)];
+        double tau  = parameters[baseIndex + (independentBits ? i*3+2 : 0)];
+        if(t >= t0)
+          value += conc * exp(-(t - t0)/tau);
+      }
+      break;
+    default:
+      ;
+    }
+    return value;
+  };
+
+  /// Parses a spec for the time-based stuff. Takes a string of the
+  /// form number, type, common
+  void parseFromString(const QString & str) {
+    
+    QRegExp parse("^\\s*(\\d+)\\s*,\\s*(\\w+)\\s*(,\\s*common)?\\s*$");
+    if(parse.indexIn(str) != 0)
+      throw RuntimeError(QString("Invalid specification for time "
+                                 "dependence: '%1'").arg(str));
+    if(parse.cap(2) != "exp")
+      throw RuntimeError(QString("Invalid type of time "
+                                 "dependence: '%1'").arg(parse.cap(2)));
+    
+    type = Exponential;
+    number = parse.cap(1).toInt();
+    
+    independentBits = parse.cap(3).isEmpty();
+    
+  };
+  
+  
+};
+
+
 /// Fits a full kinetic system.
 class KineticSystemFit : public PerDatasetFit {
 
   /// System we're working on - deleted and recreated for each fit.
   KineticSystem * system;
 
-  /// Index of the current's system index for the time
+  /// The evolver... Created and delete at the same time as each fit.
+  KineticSystemEvolver * evolver;
+
+  /// Index of the current's system index for the time. @hack Drop the
+  /// mutable by moving parameter computation somewhere else.
   mutable int timeIndex;
-  
+
+  /// Time-dependent parameters !
+  QHash<int, TimeDependentParameter> timeDependentParameters;
+
+  /// For now, a rather hackish substitute for callbacks with
+  /// parameters
+  const double * params;
+
+  /// Base index for the time-dependent stuff. @hack Drop the mutable
+  mutable int tdBase;
+
+  /// Parameter lists for the time-dependent stuff
+  QList<ParameterDefinition> tdParameters;
 
 protected:
 
   virtual void processOptions(const CommandOptions & opts)
   {
-    // For now, no extra stuff !
+    // Options get processed when everything else is done.
+
+    QStringList lst;
+    updateFromOptions(opts, "with", lst);
+
+    int baseIndex = 0;
+    tdParameters.clear();
+
+    for(int i = 0; i < lst.size(); i++) {
+      Terminal::out << "Parsing spec: " << lst[i] << endl;
+      QStringList s2 = lst[i].split(":");
+      if(s2.size() != 2)
+        throw RuntimeError("Time-dependent parameter '%1' "
+                           "should contain a single :").
+          arg(lst[i]);
+      int idx = evolver->getParameterIndex(s2[0]);
+      if(idx < 0)
+        throw RuntimeError("Unknown parameter: %1").arg(s2[0]);
+      timeDependentParameters[idx] = TimeDependentParameter();
+      TimeDependentParameter & param = timeDependentParameters[idx];
+      param.parseFromString(s2[1]);
+      param.baseIndex = baseIndex;
+      QList<ParameterDefinition> defs = param.parameters(s2[0]);
+      baseIndex += defs.size();
+      tdParameters += defs;
+    }
   }
 
   void runFit(const QString & name, QString fileName, 
@@ -243,9 +389,11 @@ protected:
               const CommandOptions & opts)
   {
     delete system;
+    delete evolver;
     system = new KineticSystem;
     system->parseFile(fileName);
     system->prepare();
+    evolver = new KineticSystemEvolver(system);
     Fit::runFit(name, datasets, opts);
   }
 
@@ -273,12 +421,16 @@ public:
 
     for(int i = 0; i < parameters.size(); i++) {
       if(parameters[i] == "t") {
-        timeIndex = i;
+        timeIndex = i; /// @hack modifying a mutable stuff. Move the
+                       /// preparation of the parameter list in its
+                       /// own function after options processing.
         continue;
       }
       defs << ParameterDefinition(parameters[i], 
                                   i < system->speciesNumber()); 
     }
+    tdBase = defs.size();
+    defs += tdParameters;
     return defs;
   };
 
@@ -288,26 +440,27 @@ public:
     if(fit->timeIndex >= 0)
       params[fit->timeIndex] = t;
 
-    // Now, must handle the steps.
+    for(QHash<int, TimeDependentParameter>::const_iterator i = 
+          fit->timeDependentParameters.begin(); 
+        i != fit->timeDependentParameters.end(); i++)
+      params[i.key()] = i.value().computeValue(t, fit->params);
   }
 
   virtual void function(const double * a, FitData * data, 
                         const DataSet * ds , gsl_vector * target)
   {
-    KineticSystemEvolver evolver(system);
-    evolver.setParameters(a + system->speciesNumber(), 
+    evolver->setParameters(a + system->speciesNumber(), 
                           timeIndex);
-    evolver.setupCallback(KineticSystemFit::timeDependentRates, this);
 
+    evolver->setupCallback(KineticSystemFit::timeDependentRates, this);
     const Vector & xv = ds->x();
-    evolver.initialize(xv[0]);
-
-    /// @todo TIME !
+    evolver->initialize(xv[0]);
+    params = a + tdBase;///  @hack this looks pretty hackish to me...
 
     for(int i = 0; i < xv.size(); i++) {
-      evolver.stepTo(xv[i]);
+      evolver->stepTo(xv[i]);
       double val = 0;
-      const double * cv = evolver.currentValues();
+      const double * cv = evolver->currentValues();
       for(int j = 0; j < system->speciesNumber(); j++)
         val += cv[j] * a[j];
       gsl_vector_set(target, i, val);
@@ -341,7 +494,7 @@ public:
   KineticSystemFit() :
     PerDatasetFit("kinetic-system", 
                   "Full kinetic system",
-                  "", 1, -1, false) 
+                  "", 1, -1, false), system(NULL), evolver(NULL)
   { 
 
     ArgumentList * al = new 
@@ -353,6 +506,10 @@ public:
 
     ArgumentList * opts = new 
       ArgumentList(QList<Argument *>()
+                   << new SeveralStringsArgument("with", 
+                                                 "Time dependent parameters",
+                                                 "Dependency upon time of "
+                                                 "various parameters")
                    );
 
     makeCommands(al, 

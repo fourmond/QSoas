269730269656aae83ded3c26ba787646864e4b8a
Vincent Fourmond <vincent.fourmond@imm.cnrs.fr>
2012-11-14 11:48:41 +0100

    Improved the nerstian fit to accomodate for an arbitrary large number of species

--- src/echem-fits.cc
+++ src/echem-fits.cc
@@ -1,7 +1,7 @@
 /**
    @file echem-fits.cc
-   Fits for electrochemists
-   Copyright 2011 by Vincent Fourmond
+   Fits related to electrochemistry
+   Copyright 2011, 2012 by Vincent Fourmond
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -31,43 +31,119 @@
 
 #include <gsl/gsl_const_mksa.h>
 
-/// Nernstian fit
+/// Fit for a full nerstian system.
 class NernstFit : public FunctionFit {
+
+  /// The number of species
+  int number;
+
+  /// The species which should be considered "unstable"
+  ///
+  /// @todo I should come up with a simple for handling boolean values
+  /// indexed on a small number of integers - using bit fields; that
+  /// would most probably be faster and definitely much more compact
+  /// than a set !
+  QSet<int> unstableSpecies;
+
+protected:
+
+  virtual void processOptions(const CommandOptions & opts)
+  {
+    number = 2;
+    updateFromOptions(opts, "species", number);
+
+    QList<int> unstable;
+    /// @todo Implement unstable species !
+    updateFromOptions(opts, "unstable", unstable);
+    unstableSpecies = QSet<int>::fromList(unstable);
+
+  }
+
+  
+  virtual QString optionsString() const {
+    return QString("%1 species").
+      arg(number);
+  }
+
+
 public:
 
+  /// Numbering: 0 is the most reduced species.
+  virtual QList<ParameterDefinition> parameters() const {
+    QList<ParameterDefinition> defs;
 
-  /// Formula:
+    defs << ParameterDefinition("temperature", true);
+    
+    // Absorbances
+    for(int i = 0; i < number; i++)
+      defs << ParameterDefinition(QString("A_%1").arg(i));
+    
+    for(int i = 0; i < number-1; i++) {
+      defs << ParameterDefinition(QString("E_%1/%2").arg(i+1).arg(i));
+      defs << ParameterDefinition(QString("n_%1/%2").arg(i+1).arg(i), true);
+    }
+    return defs;
+  };
+
   virtual double function(const double * a, 
                           FitData * , double x) {
-    double f = GSL_CONST_MKSA_FARADAY /(a[0] * GSL_CONST_MKSA_MOLAR_GAS);
-    double e = exp(a[1] * f * (x - a[2]));
-    return (a[3] + a[4] * e)/(1 + e);
+    double rel = 1;             // Relative amount of species
+    double numer = 0;           // Sum for the numerator
+    double denom = 0;           // Sum for the denominator
+
+
+    double fara = GSL_CONST_MKSA_FARADAY /
+      (a[0] * GSL_CONST_MKSA_MOLAR_GAS);
+
+    const double * ampl = a+1;
+    const double * couples = ampl + number;
+
+    for(int i = 0; i < number; i++) {
+      if(i > 0) {
+        rel *= exp( fara * (x - couples[0])/couples[1]);
+        couples += 2;
+      }
+      denom += rel;
+      numer += *ampl * rel;
+      ++ampl;
+    }
+    return numer/denom;
   };
 
-  virtual void initialGuess(FitData * , 
-                            const DataSet * ds,
+  virtual void initialGuess(FitData * params, 
+                            const DataSet *ds,
                             double * a)
   {
-    a[0] = soas().temperature();   a[1] = 1;
-    a[2] = 0.5*(ds->x().min() + ds->x().max());
-    a[3] = ds->y().first();
-    a[4] = ds->y().last();
+    double *t = a-1;
+    *(++t) = soas().temperature();
+    const double ymin = ds->y().min();
+    const double ymax = ds->y().max();
+    for(int i = 0; i < number; i++)
+      *(++t) = ymin + i *(ymax - ymin)/(number-1);
+    
+    // Now transitions
+    const double xmin = ds->x().min();
+    const double xmax = ds->x().max();
+    for(int i = 0; i < number-1; i++) {
+      *(++t) = xmin + (i+0.5) *(xmax - xmin)/(number-1);
+      *(++t) = 1;
+    }
   };
-
-  virtual QList<ParameterDefinition> parameters() const {
-    return QList<ParameterDefinition>()
-      << ParameterDefinition("temperature", true)
-      << ParameterDefinition("n", true)
-      << ParameterDefinition("E0")
-      << ParameterDefinition("ared")
-      << ParameterDefinition("aox");
-  };
-
-
+  
+  
   NernstFit() : FunctionFit("nernst", 
                             "Nerstian behaviour",
-                            "Fit to a Nerst equation") 
-  { ;};
+                            "Fit to a Nerst equation", 1, -1, false) 
+  { 
+    ArgumentList * opts = new 
+      ArgumentList(QList<Argument *>()
+                   << new IntegerArgument("species", 
+                                          "Number of species",
+                                          "Number of redox species")
+                   );
+    makeCommands(NULL, NULL, NULL, opts);
+
+  };
 };
 
 // DO NOT FORGET TO CREATE AN INSTANCE OF THE CLASS !!

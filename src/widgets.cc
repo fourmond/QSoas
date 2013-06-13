f5e74c892e6b417a5fa40d32dffd5d3c9dc0d3dd
Vincent Fourmond <vincent.fourmond@9online.fr>
2013-06-14 00:55:05 +0200

    General-purpose widgets

---
+++ src/widgets.cc
@@ -0,0 +1,56 @@
+/*
+  widgets.cc: implementation of general purpose widgets
+  Copyright 2013 by Vincent Fourmond
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <headers.hh>
+#include <widgets.hh>
+
+LabeledEdit::LabeledEdit(const QString & l, const QString & c, 
+                         QWidget * parent) :
+  QWidget(parent) {
+  QHBoxLayout * layout = new QHBoxLayout(this);
+  _label = new QLabel(l);
+  layout->addWidget(_label);
+
+  _edit = new QLineEdit(c);
+  layout->addWidget(_edit);
+}
+
+void LabeledEdit::setLabelText(const QString & str)
+{
+  _label->setText(str);
+}
+
+void LabeledEdit::setText(const QString & txt)
+{
+  _edit->setText(txt);
+}
+
+QString LabeledEdit::text() const
+{
+  return _edit->text();
+}
+
+QLabel * LabeledEdit::label()
+{
+  return _label;
+}
+
+QLineEdit * LabeledEdit::lineEdit()
+{
+  return _edit;
+}
---
+++ src/widgets.hh
@@ -0,0 +1,53 @@
+/**
+   \file widgets.hh
+   A few general-purpose widgets
+   Copyright 2013 by Vincent Fourmond
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <headers.hh>
+#ifndef __WIDGETS_HH
+#define __WIDGETS_HH
+
+/// A line edit with a label !
+class LabeledEdit : public QWidget {
+  Q_OBJECT;
+
+  /// The underlying label
+  QLabel * _label;
+
+  /// The line edit
+  QLineEdit * _edit;
+public:
+  LabeledEdit(const QString & label, const QString & contents = "", 
+              QWidget * parent = NULL);
+
+  /// Changes the label
+  void setLabelText(const QString & str);
+
+  /// Changes the edit text
+  void setText(const QString & txt);
+
+  /// Gets the edit text
+  QString text() const;
+
+  /// Provides direct access to the label
+  QLabel * label();
+
+  /// Provides direct access to the line edit
+  QLineEdit * lineEdit();
+};
+
+#endif

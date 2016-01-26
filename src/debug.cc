7632300b092eb6f578c9faf0154a49e7da666039
Vincent <vincent.fourmond@9online.fr>
2016-01-26 23:13:21 +0100

    First draft of a decent debugging facility

--- src/debug.cc
+++ src/debug.cc
@@ -1,6 +1,7 @@
 /*
   debug.cc: various utility functions for debugging
   Copyright 2011 by Vincent Fourmond
+            2016 by CNRS/AMU
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -20,10 +21,129 @@
 #include <headers.hh>
 #include <debug.hh>
 
+#include <utils.hh>
+#include <exceptions.hh>
+#include <soas.hh>
+#include <datastack.hh>
+
 void Debug::dumpCurrentFocus(const QString & str)
 {
   QTextStream o(stdout);
   QWidget * w = QApplication::focusWidget();
   o << str << w << "(" 
     << (w ? w->metaObject()->className() : "-none-") << ")" << endl;
+}
+
+Debug::Debug() :
+  level(0), directory(NULL), outputDevice(NULL), output(NULL)
+{
+}
+
+Debug * Debug::theDebug = NULL;
+
+
+Debug & Debug::debug()
+{
+  if(! theDebug)
+    theDebug = new Debug;
+  return *theDebug;
+}
+
+int Debug::debugLevel()
+{
+  return debug().level;
+}
+
+
+void Debug::closeDirectory()
+{
+  if(output) {
+    delete output;
+    output = NULL;
+  }
+  if(outputDevice) {
+    delete outputDevice;
+    outputDevice = NULL;
+  }
+  if(directory) {
+    delete directory;
+    directory = NULL;
+  }
+}
+
+
+void Debug::openDirectory(const QString & dir)
+{
+  closeDirectory();
+  if(! dir.isEmpty()) {
+    directory = new QDir(dir);
+    if(! directory->makeAbsolute()) {
+      closeDirectory();
+      throw RuntimeError("Could not transform '%1' into an absolute path").
+        arg(dir);
+    }
+    QString fp = directory->absolutePath();
+    if(! directory->exists(".")) {
+      QDir rt = QDir::root();
+      if(! rt.mkpath(fp)) {
+        closeDirectory();
+        throw RuntimeError("Could not create directory '%1'").
+          arg(fp);
+      }
+    }
+    else {
+      QFileInfo info(fp);
+      if(! info.isDir()) {
+        closeDirectory();
+        throw RuntimeError("'%1' exists and is not a directory").
+          arg(fp);
+      }
+    }
+  }
+
+  outputDevice = new QFile(directory->absoluteFilePath("debug.log"));
+  Utils::open(outputDevice, QIODevice::ReadOnly);
+  /// @bug leaks memory
+  output = new QTextStream(outputDevice);
+}
+
+
+
+void Debug::startCommand(const QStringList & cmdline)
+{
+  if(level > 0 && directory) {
+    timeStamp();
+    (*this) << "Starting command: '" << cmdline.join("'")
+            << "'\nRotating stack file" << endl;
+    Utils::rotateFile(directory->absoluteFilePath("stack.bin"), 9);
+  }
+}
+
+void Debug::endCommand(const QStringList & cmdline)
+{
+  if(level > 0 && directory) {
+    timeStamp();
+    (*this) << "Finishing command: '" << cmdline.join("'")
+            << "'" << endl;
+    saveStack();
+  }
+}
+
+void Debug::saveStack()
+{
+  if(level > 0 && directory) {
+    timeStamp();
+    (*this) << "Saving stack" << endl;
+    QFile file(directory->absoluteFilePath("stack.bin"));
+    Utils::open(&file, QIODevice::WriteOnly);
+    QDataStream out(&file);
+    out << soas().stack();
+  }
+}
+
+
+void Debug::timeStamp()
+{
+  QDateTime dt = QDateTime::currentDateTime();
+  (*this) << "[" << dt.toString() << "] ";
 }
--- src/debug.hh
+++ src/debug.hh
@@ -2,6 +2,7 @@
    \file debug.hh
    Code useful for debugging.
    Copyright 2011 by Vincent Fourmond
+             2016 by CNRS/AMU
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -22,8 +23,82 @@
 #ifndef __DEBUG_HH
 #define __DEBUG_HH
 
-/// Functions useful for debugging.
-namespace Debug {
+/// Class handling all the debugging in QSoas.
+///
+/// It handles:
+/// @li all debug output, written to standard output (anyway), and to
+/// a dedicated log file if available
+/// @li saving and rotating stacks at each command (and each dataset
+/// addition).
+class Debug {
+
+  /// The current debug level
+  int level;
+
+  /// The current debug directory, or NULL if there isn't such a thing
+  ///
+  /// This is the directory in which the stacks are saved and the
+  /// debug output is.
+  QDir * directory;
+
+  /// The underlying output device
+  QFile * outputDevice;
+
+  /// The output log file.
+  QTextStream * output;
+
+  /// This is the standard output.
+  QTextStream out;
+
+  /// The instance of debug.
+  static Debug * theDebug;
+
+protected:
+  void timeStamp();
+  
+public:
+
+  Debug();
+
+  /// Returns the static instance
+  static Debug & debug();
+
+  /// Returns the current debug level
+  static int debugLevel();
+
+  /// Sets up the debug object to work with the given directory.
+  ///
+  /// If @a directory is empty, then the directory is just closed, and
+  /// a new one is not opened.
+  ///
+  /// If the directory does not exist, it is created.
+  void openDirectory(const QString & directory);
+
+  /// Returns the full path of the target directory, or an empty
+  /// string if there isn't.
+  QString directoryPath() const;
+
+  /// Closes the current directory
+  void closeDirectory();
+
+  /// Marks the beginnings of the processing of a command.
+  void startCommand(const QStringList & cmdline);
+
+  /// Marks the end of the given command.
+  void endCommand(const QStringList & cmdline);
+
+  /// Saves the stack, if the debug level is above 0 and there is a
+  /// target directory.
+  void saveStack();
+
+
+  template<class T> Debug& operator<<(T t);
+  
+  /// @name Helper functions
+  ///
+  /// A series of useful functions for debugging.
+  ///
+  /// @{
 
   /// Writes some information about the widget currently holding the
   /// focus to standard output.
@@ -44,6 +119,15 @@ namespace Debug {
       arg(rect.isValid() ? "valid" : "invalid").
       arg(rect.isEmpty() ? "empty" : "non-empty");
   };
+
+  /// @}
 };
+
+template<class T> inline  Debug& Debug::operator<<(T t) {
+  out << t;
+  if(output)
+    *output << t;
+  return *this;
+}
 
 #endif

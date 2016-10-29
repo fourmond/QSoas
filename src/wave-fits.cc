0acee7d80603336dc98464f84a7fad6d8648bd53
Vincent <vincent.fourmond@9online.fr>
2016-10-30 00:05:46 +0200

    Now switch to oxidation by default + parameters name change

--- src/wave-fits.cc
+++ src/wave-fits.cc
@@ -731,11 +731,13 @@ protected:
   {
     Storage * s = storage<Storage>(data);
     s->approx = Dispersion;
-    s->isOxidation = false;
     s->useEoc = false;
+
+    bool reduction = false;
     updateFromOptions(opts, "model", s->approx);
-    updateFromOptions(opts, "oxidation", s->isOxidation);
+    updateFromOptions(opts, "reduction", reduction);
     updateFromOptions(opts, "use-eoc", s->useEoc);
+    s->isOxidation = ! reduction;
   }
 
   
@@ -753,13 +755,17 @@ public:
     Storage * s = storage<Storage>(data);
     QList<ParameterDefinition> defs;
 
+    const char * knm1 = (s->isOxidation ? "kox/k0or" : "kred/k0or");
+    const char * knm2 = (s->isOxidation ? "kred/kox" : "kox/kred");
+
     defs << ParameterDefinition("temperature", true)
-         << ParameterDefinition("E1")
-         << ParameterDefinition("E2");
+         << ParameterDefinition("Eoi")
+         << ParameterDefinition("Eir");
     if(s->useEoc)
       defs << ParameterDefinition("Eoc");
     else
-      defs << ParameterDefinition("k-2/k2");
+      defs << ParameterDefinition(knm2);
+        
     switch(s->approx) {
     case Nernst:
       defs << ParameterDefinition("ilim");
@@ -767,15 +773,15 @@ public:
     case SlowET:
     case Full:
       defs << ParameterDefinition("ilim")
-           << ParameterDefinition("k2/k01")
-           << ParameterDefinition("k01/k02");
+           << ParameterDefinition(knm1)
+           << ParameterDefinition("k0oi/k0ir");
       if(s->approx == Full)
         defs << ParameterDefinition("betad0");
           break;
     case Dispersion:
       defs << ParameterDefinition("ilim/betad0")
-           << ParameterDefinition("k2/k01")
-           << ParameterDefinition("k01/k02");
+           << ParameterDefinition(knm1)
+           << ParameterDefinition("k0oi/k0ir");
 
       break;
     }
@@ -831,12 +837,13 @@ public:
 
     const double E1 = params[1];
     const double E2 = params[2];
-    double k_m2_k2 = params[3];
+    double k_bias = params[3];
 
     if(s->useEoc)
-      k_m2_k2 = exp(2*f * (0.5*(E1+E2) - k_m2_k2));
+      k_bias = exp((s->isOxidation ? -2 : 2)*f * (0.5*(E1+E2) - k_bias));
+    
 
-    if(k_m2_k2 < 0)
+    if(k_bias < 0)
       throw RangeError("Negative bias");
 
     
@@ -847,9 +854,15 @@ public:
     if(k2_k0 < 0)
       throw RangeError("Negative k2/k01");
 
-    double k01_k02 = (s->approx == Nernst ? 0 : params[6]);
-    if(k01_k02 < 0)
-      throw RangeError("Negative k01/k02");
+    double k0oi_k0ir_hlf;
+    {
+      const double k01_k02 = (s->approx == Nernst ? 0 : params[6]);
+      if(k01_k02 < 0)
+        throw RangeError("Negative k0oi/k0ir");
+      
+      k0oi_k0ir_hlf = sqrt(k01_k02);
+    }
+
 
     double bd0 = 0;
     if(s->approx == Full) {
@@ -869,28 +882,39 @@ public:
 
     for(int i = 0; i < xv.size(); i++) {
       double x = xv[i];
-      x -= E1;
+      x -= E1;                  // = E_OI
       if(s->isOxidation)
         x = -x;
       double d1 = exp(0.5 * f * x);
       double e1 = d1 * d1;
 
       x = xv[i];
-      x -= E2;
+      x -= E2;                  // = E_IR
       if(s->isOxidation)
         x = -x;
       double d2 = exp(0.5 * f * x);
       double e2 = d2 * d2;
 
-      double a = 1 + e2 * (1 + e1);
-      double ap = k_m2_k2 * e1 * e2;
+      // For the case of oxidation, e and d are already inverted
+
+
+      double a = s->isOxidation ?
+        1 + e1*(1 + e2)
+        : 1 + e2*(1 + e1)
+        ;
+      double ap = k_bias * e1 * e2;
       double v;
       if(s->approx == Nernst) {
         v = cur * (1 - ap)/a;
       }
       else {
-        double b = d1 * (1 + k_m2_k2 * (1 + e2)) +
-          k01_k02 * d2 * (1 + (1 + k_m2_k2) * e1);
+        double b = s->isOxidation ?
+          d1/k0oi_k0ir_hlf * (1 + (1 + k_bias) * e2) +
+          k0oi_k0ir_hlf * d2 * (1 + k_bias*(1+e1))
+          :
+          d1/k0oi_k0ir_hlf * (1 + k_bias*(1+e2)) +
+          k0oi_k0ir_hlf * d2 * (1 + (1 + k_bias) * e1);
+          ;
         switch(s->approx) {
         case SlowET:
           v = cur * (1 - ap)/(a + b * k2_k0);
@@ -921,9 +945,9 @@ public:
                     "Model", 
                     "the kind of model used for the computation (default: dispersion)")
                    << new 
-                   BoolArgument("oxidation", 
-                                "Reference is oxidation",
-                                "if on, use the oxidation current as reference (default: off, reductive current as reference)")
+                   BoolArgument("reduction", 
+                                "Reference is reduction",
+                                "if on, use the reductive direction as reference (default: oxidative direction as reference)")
                    << new 
                    BoolArgument("use-eoc", 
                                 "Use open circuit",

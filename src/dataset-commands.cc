6e48796a74d21360b96a5824868b94f9a1fdeed6
Vincent Fourmond <vincent.fourmond@imm.cnrs.fr>
2018-01-16 13:08:15 +0100

    Fix a missing break, and rearrange so that mid with cu picks AND write

--- src/dataset-commands.cc
+++ src/dataset-commands.cc
@@ -655,6 +655,7 @@ namespace __cu {
     QuitSubtractingRespRef,
     QuitDividingRespRef,
     WriteToOutput,
+    PickAndWrite,
     ShiftX,
     ScaleX,
     ShiftY,
@@ -666,6 +667,8 @@ namespace __cu {
   static EventHandler cursorHandler = EventHandler("cursor").
     addClick(Qt::LeftButton, PickPoint, "place cursor").
     addClick(Qt::RightButton, PickRef, "place reference").
+    addKey(' ', WriteToOutput, "write to output").
+    addClick(Qt::MiddleButton, PickAndWrite, "place and write to output").
     addKey('v', QuitDividing, "quit dividing by Y value").
     alsoKey('V').
     addKey('u', QuitSubtracting, "quit subtracting Y value").
@@ -679,9 +682,7 @@ namespace __cu {
     addPointPicker().
     addKey(Qt::Key_Escape, Abort, "abort").
     addKey('q', Quit, "quit").
-    alsoKey('Q').
-    addClick(Qt::MiddleButton, WriteToOutput, "write to output").
-    alsoKey(' ');
+    alsoKey('Q');
 
 
 
@@ -725,7 +726,8 @@ static void cursorCommand(CurveEventLoop &loop, const QString &)
     int action = cursorHandler.nextAction(loop);
     pick.processEvent(action);
     switch(action) {
-    case PickPoint: {
+    case PickPoint:
+    case PickAndWrite: {
       m.p = pick.point();
       e.clear();
       int idx = pick.pointIndex();
@@ -745,8 +747,16 @@ static void cursorCommand(CurveEventLoop &loop, const QString &)
         Terminal::out << e.keyOrder.join("\t") << endl;
       }
       Terminal::out << e.toString() << endl;
-      break;
+      if(action == PickPoint)
+        break;
     }
+    case WriteToOutput:
+      e.prepend("buffer", ds->name);
+      Terminal::out << "Writing position to output file: '" 
+                    << OutFile::out.fileName() << "'" << endl;
+      
+      OutFile::out.writeValueHash(e, ds);
+      break;
     case PickRef:
       r.p = pick.point();
       Terminal::out << "Reference:\t"  << r.p.x() << "\t" 
@@ -784,12 +794,6 @@ static void cursorCommand(CurveEventLoop &loop, const QString &)
       soas().pushDataSet(ds->derivedDataSet(ny, "_div.dat"));
       return;
     }
-    case WriteToOutput:
-      e.prepend("buffer", ds->name);
-      Terminal::out << "Writing position to output file: '" 
-                    << OutFile::out.fileName() << "'" << endl;
-      
-      OutFile::out.writeValueHash(e, ds);
     case ShiftX: {
       double dx = m.p.x() - r.p.x();
       if(! std::isfinite(dx)) {
